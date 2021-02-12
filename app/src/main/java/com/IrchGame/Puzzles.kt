344b14f667f207b15e2544d4dcb8cac28bed74ff
Solovyev Maksim <maksim.solovev02@mail.ru>
2021-02-13 01:09:38 +0300

    Добавил новые окна и переход по кнопкам

---
+++ app/src/main/java/com/IrchGame/Puzzles.kt
@@ -0,0 +1,11 @@
+package com.IrchGame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Puzzles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puzzles)
+    }
+}
---
+++ app/src/main/java/com/IrchGame/StoryMap.kt
@@ -0,0 +1,11 @@
+package com.IrchGame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoryMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_map)
+    }
+}
