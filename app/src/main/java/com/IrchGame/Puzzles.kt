495332399ba02137f9bfd28e6a5c277bdfb8fc95
Solovyev Maksim <maksim.solovev02@mail.ru>
2021-02-25 20:22:08 +0300

    добавил игру с разделением

--- app/src/main/java/com/IrchGame/Puzzles.kt
+++ app/src/main/java/com/IrchGame/Puzzles.kt
@@ -1,11 +1,18 @@
 package com.IrchGame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Puzzles : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzles)
+    }
+
+    fun open_separation(view: View) {
+        val window = Intent(this, Separation_Puzzle::class.java)
+        startActivity(window)
     }
 }
