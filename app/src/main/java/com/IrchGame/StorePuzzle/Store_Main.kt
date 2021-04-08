952818d351d21843fb20fe168cc7870b662c224f
DevSquire <teacher.1cb@yandex.ru>
2021-04-08 17:35:53 +0300

    added onClick function for buttons

--- app/src/main/java/com/IrchGame/StorePuzzle/Store_Main.kt
+++ app/src/main/java/com/IrchGame/StorePuzzle/Store_Main.kt
@@ -4,24 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TextView
 import com.IrchGame.R
 import kotlinx.android.synthetic.main.activity_store_puzzle.*
 
 class Store_Main() : AppCompatActivity() {
-    private lateinit var basketImages: Map<String, List<ImageView>>;
-    private var countOfDonuts = 5;
+    private lateinit var basketImages: Map<Int, List<ImageView>>;
+    private lateinit var counters: Map<Int, TextView> ;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store_puzzle)
+        counters = mapOf(
+                R.id.btnCookie_1 to  text_cookie_1,
+                R.id.btnCookie_2 to text_cookie_2,
+                R.id.btnWaffle_2 to text_waffle_2,
+                R.id.btnKeks to text_keks
+        )
         basketImages = mapOf(
-                "cookie_1" to  listOf(image_1_cookie_1, image_2_cookie_1, image_3_cookie_1, image_4_cookie_1, image_5_cookie_1),
-                "cookie_2" to listOf(image_1_cookie_2, image_3_cookie_2, image_2_cookie_2),
-                "waffle_1" to listOf(image_1_waffle_1, image_2_waffle_1, image_3_waffle_1, image_4_waffle_1),
-                "keks" to listOf(image_1_keks_1, image_2_keks_1, image_3_keks_1, image_4_keks_1)
+                R.id.btnCookie_1 to  listOf(image_1_cookie_1, image_2_cookie_1, image_3_cookie_1, image_4_cookie_1, image_5_cookie_1),
+                R.id.btnCookie_2 to listOf(image_1_cookie_2, image_2_cookie_2, image_3_cookie_2),
+                R.id.btnWaffle_2 to listOf(image_1_waffle_2, image_2_waffle_2, image_3_waffle_2, image_4_waffle_2),
+                R.id.btnKeks to listOf(image_1_keks_1, image_2_keks_1, image_3_keks_1, image_4_keks_1)
         )
 //        countOfDonutsText.setText(countOfDonuts.toString())
-        for(listsValue in basketImages.values){
+        for((id, listsValue) in basketImages){
+            counters[id]?.setText("0/" + listsValue.size)
             for(image in listsValue){
                 image.visibility = View.INVISIBLE;
             }
@@ -29,13 +36,22 @@ class Store_Main() : AppCompatActivity() {
 
     }
 
-    fun onCookie_1(view: View){
-        basketImages["cookie_1"]?.get(0)?.visibility =  View.VISIBLE;
+    fun onClick(view: View){
+        var btnId = view.id
+        if(btnId in basketImages.keys) {
+            var count = 0
+            for (img in basketImages[btnId]!!){
+                if((img.visibility == View.INVISIBLE)){
+                    img.visibility = View.VISIBLE
+                    count++;
+                    counters[btnId]?.setText(count.toString() + "/" + basketImages[btnId]?.size)
+                    break;
+                }
+                count++;
+            }
+        }
     }
 
     private fun checkEndGame() {
-        if(countOfDonuts == 0){
-//            congratsText.visibility = View.VISIBLE
-        }
     }
 }
