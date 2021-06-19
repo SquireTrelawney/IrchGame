c40190b8609ebf523d6b70a5e866ee4e6b4421c0
Solovyev Maksim <maksim.solovev02@mail.ru>
2021-06-19 22:09:23 +0300

    corrected the ice cream

--- app/src/main/java/com/IrchGame/Separation_Puzzle.kt
+++ app/src/main/java/com/IrchGame/Separation_Puzzle.kt
@@ -3,16 +3,11 @@ package com.IrchGame
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
 import kotlinx.android.synthetic.main.activity_separation__puzzle.*
-import kotlinx.android.synthetic.main.activity_shadows_puzzle.*
-import java.lang.reflect.Array
-import java.util.*
 import kotlin.math.abs
 import kotlin.random.Random
 
@@ -29,17 +24,20 @@ class Separation_Puzzle : AppCompatActivity(), GestureDetector.OnGestureListener
     var y2:Float = 0.0f
     var y1:Float = 0.0f
 
-    var obj1: img = img("l", R.drawable.slon_8)
-    var obj2: img = img("l", R.drawable.zmeya_8)
-    var obj3: img = img("l", R.drawable.znak)
-    var obj4: img = img("r", R.drawable.indus)
-    var obj5: img = img("r", R.drawable.ind)
-    var obj6: img = img("r", R.drawable.og)
-    var obj7: img = img("n", R.drawable.tuk)
+    var obj1: img = img("l", R.drawable.ice_fon4)
+    var obj2: img = img("l", R.drawable.ice_fon5)
+    var obj3: img = img("l", R.drawable.ice_fon6)
+    var obj4: img = img("r", R.drawable.ice_fon1)
+    var obj5: img = img("r", R.drawable.ice_fon2)
+    var obj6: img = img("r", R.drawable.ice_fon3)
     var mas = arrayOf(obj1, obj2, obj3, obj4, obj5, obj6)
 
-    var mid_image = img("left", R.drawable.slon_8)
-    var score = 1
+    var lives = 3
+
+    var score = 0
+
+    var score_right = 0
+    var score_left = 0
 
     companion object{
         const val MIN_DISTANCE = 150
@@ -86,65 +84,19 @@ class Separation_Puzzle : AppCompatActivity(), GestureDetector.OnGestureListener
         return mas_out
     }
 
-//    var timer = object: CountDownTimer(20000, 1000) {
-//        override fun onTick(millisUntilFinished: Long) {
-//            val cdt = millisUntilFinished.toInt() / 1000
-//            val result = Math.round(cdt.toDouble())
-//            TextView4.text = result.toString()
-//        }
-//        override fun onFinish() {
-//            TextView.text = "GAME OVER"
-//            game_over()
-//        }
-//    }
-
-//    fun restart_timer(score: Long) {
-//        println("Cчёт " + score)
-//        if (score <= 17.toLong()){
-//            timer = object : CountDownTimer(20000 - (score*250), 1000) {
-//                override fun onTick(millisUntilFinished: Long) {
-//                    val cdt = millisUntilFinished.toInt() / 1000
-//                    val result = Math.round(cdt.toDouble())
-//                    TextView.text = result.toString()
-//                }
-//
-//                override fun onFinish() {
-//                    TextView.text = "GAME OVER"
-//                    game_over()
-//                }
-//            }
-//        }
-//        else{
-//            timer = object : CountDownTimer(2000, 1000) {
-//                override fun onTick(millisUntilFinished: Long) {
-//                    val cdt = millisUntilFinished.toInt() / 1000
-//                    val result = Math.round(cdt.toDouble())
-//                    TextView.text = result.toString()
-//                }
-//
-//                override fun onFinish() {
-//                    TextView.text = "GAME OVER"
-//                    game_over()
-//                }
-//            }
-//        }
-//        timer.start()
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_separation__puzzle)
-//        timer.start()
-        imageView6.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.ind_8))
-        imageView7.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.indus_8))
-//        TextScore2.text = score.toString()
-        update_img(imageView5, mid_image)
+
 
         mas = fill(obj1, obj2, obj3, obj4, obj5, obj6)
 
         imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mas[score].img))
 
+
         gestureDetector = GestureDetector(this, this)
+
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -165,19 +117,19 @@ class Separation_Puzzle : AppCompatActivity(), GestureDetector.OnGestureListener
 
                 if ((abs(valueX) > MIN_DISTANCE) and (game == true)) {
                     if (x2 > x1) {
-                        Toast.makeText(this, "Right swipe", Toast.LENGTH_SHORT).show()
-                        println("Right")
+//                        Toast.makeText(this, "Right swipe", Toast.LENGTH_SHORT).show()
+//                        println("Right")
                         switch_right()
                     } else {
-                        Toast.makeText(this, "Left swipe", Toast.LENGTH_SHORT).show()
-                        println("Left")
+//                        Toast.makeText(this, "Left swipe", Toast.LENGTH_SHORT).show()
+//                        println("Left")
                         switch_left()
                     }
                 } else if ((abs(valueX) > MIN_DISTANCE) and (game == true)) {
                     if (y2 > y1) {
-                        Toast.makeText(this, "Bottom swipe", Toast.LENGTH_SHORT).show()
+//                        Toast.makeText(this, "Bottom swipe", Toast.LENGTH_SHORT).show()
                     } else {
-                        Toast.makeText(this, "Top swipe", Toast.LENGTH_SHORT).show()
+//                        Toast.makeText(this, "Top swipe", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -224,93 +176,115 @@ class Separation_Puzzle : AppCompatActivity(), GestureDetector.OnGestureListener
         return false
     }
 
-
-
-    fun update_img(view: View, mid_image: img){
-        val random_num = Random.nextInt(from = 0, until = 2)
-        println(random_num)
-        if (random_num == 0){
-            mid_image.set(mid_image, "left", R.drawable.ind_8)
-        }
-        else{
-            mid_image.set(mid_image, "right", R.drawable.indus_8)
-        }
-        imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mid_image.img))
-    }
-
     fun switch_left(){
-        println("score = " + score)
-        if (score < 6){
-            println(mas[score].switch)
-            if(mas[score].switch == "l"){
-                score += 1
-                imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mas[score].img))
-                if (mas[score-1].img == R.drawable.slon_8){
-                    imageView14.setVisibility(View.VISIBLE)
+            if ((score < 6)) {
+                if (mas[score].switch == "l"){
+
+                    if (score < 5) {
+                        imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mas[score + 1].img))
+                    }
+                    if (score == 5) {
+                        imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.ice_fon_empty))
+                    }
+
+                    if (score_left == 0) {
+                        ice_left_1.setImageResource(mas[score].img)
+                        ice_left_1.setVisibility(View.VISIBLE)
+                    }
+                    if (score_left == 1) {
+                        ice_left_2.setImageResource(mas[score].img)
+                        ice_left_2.setVisibility(View.VISIBLE)
+                    }
+                    if (score_left == 2) {
+                        ice_left_3.setImageResource(mas[score].img)
+                        ice_left_3.setVisibility(View.VISIBLE)
+                    }
+                    score = score + 1
+                    score_left = score_left + 1
                 }
-                if (mas[score-1].img == R.drawable.zmeya_8){
-                    imageView15.setVisibility(View.VISIBLE)
-                }
-                if (mas[score-1].img == R.drawable.znak){
-                    imageView16.setVisibility(View.VISIBLE)
-                }
-                if (mas[score-1].img == R.drawable.ind){
-                    imageView17.setVisibility(View.VISIBLE)
-                }
-                if (mas[score-1].img == R.drawable.indus){
-                    imageView18.setVisibility(View.VISIBLE)
-                }
-                if (mas[score-1].img == R.drawable.og){
-                    imageView19.setVisibility(View.VISIBLE)
+                else{
+                    if (lives != 0){
+                        if (lives == 3){
+                            live1.setVisibility(View.INVISIBLE)
+                        }
+                        if (lives == 2){
+                            live2.setVisibility(View.INVISIBLE)
+                        }
+                        if (lives == 1){
+                            live3.setVisibility(View.INVISIBLE)
+                        }
+                        lives = lives - 1
+                    }
+                    if (lives == 0){
+                        game_over()
+                    }
                 }
             }
-        }
         else{
             game_over()
         }
+        println("score = " + score)
     }
 
     fun switch_right(){
-        println("score = " + score)
-        if (score < 6){
-            println(mas[score].switch)
-            if(mas[score].switch == "r"){
-                score += 1
-                imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mas[score].img))
-                if (mas[score-1].img == R.drawable.slon_8){
-                    imageView14.setVisibility(View.VISIBLE)
+        if ((score < 6)) {
+            if  (mas[score].switch == "r"){
+                if (score < 5) {
+                    imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), mas[score + 1].img))
                 }
-                if (mas[score-1].img == R.drawable.zmeya_8){
-                    imageView15.setVisibility(View.VISIBLE)
+                if (score == 5) {
+                    imageView5.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.ice_fon_empty))
                 }
-                if (mas[score-1].img == R.drawable.znak){
-                    imageView16.setVisibility(View.VISIBLE)
+
+                if (score_right == 0) {
+                    ice_right_2.setImageResource(mas[score].img)
+                    ice_right_2.setVisibility(View.VISIBLE)
                 }
-                if (mas[score-1].img == R.drawable.ind){
-                    imageView17.setVisibility(View.VISIBLE)
+                if (score_right == 1) {
+                    ice_right_3.setImageResource(mas[score].img)
+                    ice_right_3.setVisibility(View.VISIBLE)
                 }
-                if (mas[score-1].img == R.drawable.indus){
-                    imageView18.setVisibility(View.VISIBLE)
+                if (score_right == 2) {
+                    ice_right_1.setImageResource(mas[score].img)
+                    ice_right_1.setVisibility(View.VISIBLE)
                 }
-                if (mas[score-1].img == R.drawable.og){
-                    imageView19.setVisibility(View.VISIBLE)
+                score = score + 1
+                score_right = score_right + 1
+            }
+            else{
+                if (lives != 0){
+                    if (lives == 3){
+                        live1.setVisibility(View.INVISIBLE)
+                    }
+                    if (lives == 2){
+                        live2.setVisibility(View.INVISIBLE)
+                    }
+                    if (lives == 1){
+                        live3.setVisibility(View.INVISIBLE)
+                    }
+                    lives = lives - 1
+                }
+                if (lives == 0){
+                    game_over()
                 }
             }
+
         }
         else{
             game_over()
         }
+        println("score = " + score)
     }
 
     fun game_over(){
         game = false
-        if (score <= 5){
-            textView_gameover.text = "Game Over"
+        if (score <= 4){
+//            textView_gameover.text = "Game Over"
+            image_loss.setVisibility(View.VISIBLE)
         }
         else{
-            textView_gameover.text = "You Win!"
+//            textView_gameover.text = "You Win!"
+            image_win.setVisibility(View.VISIBLE)
         }
-        textView_gameover.setVisibility(View.VISIBLE)
-        imageView5.setVisibility(View.GONE)
     }
 }
